701b9ed352402d0a3660fb3ed5d31fc771a9c50e
jrry8 <31778235+jrry8@users.noreply.github.com>
2023-09-03 12:40:46 -0700

    created model for topics

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+# A model tells Django how to work with data stored in the app
+# code-wise, a model is just a class
+
+class Topic(models.Model):
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.text
