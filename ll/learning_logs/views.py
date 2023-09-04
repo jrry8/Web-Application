c8de8c991e90825b9707075b012ae7a95c62208e
jrry8 <31778235+jrry8@users.noreply.github.com>
2023-09-03 23:00:37 -0700

    added page for users to add new entries

--- ll/learning_logs/views.py
+++ ll/learning_logs/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 
 from .models import Topic
-from .forms import TopicForm
+from .forms import TopicForm, EntryForm
 
 # home page for learning log
 def index(request):
@@ -24,15 +24,36 @@ def topic(request, topic_id):
 
 # add a new topic
 def new_topic(request):
+    # when the user intially requests this page, the browser will sent a GET request
     if request.method != 'POST':
         # no data submitted; create a blank form.
         form = TopicForm()
+    # when the user submits the form, the browser will send a POST request
     else:
         # POST data submitted; process data
         form = TopicForm(request.POST)
         if form.is_valid():
+            # save() writes the data from the form to the database
             form.save()
+            # redirect the user's browser to the topics page
             return HttpResponseRedirect(reverse('learning_logs:topics'))
-
     context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
+
+# add a new entry for a particular topic
+def new_entry(request, topic_id):
+    topic = Topic.objects.get(id=topic_id)
+    if request.method != 'POST':
+        form = EntryForm()
+    else:
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            # create new entry object but without saving to the database yet
+            new_entry = form.save(commit=False)
+            # save to database after updating the entry's topic attribute
+            new_entry.topic = topic
+            new_entry.save()
+            # redirect to the relevant topic page
+            return HttpResponseRedirect(reverse('learning_logs:topic', args=[topic_id]))
+    context = {'topic': topic, 'form': form}
+    return render(request, 'learning_logs/new_entry.html', context)
