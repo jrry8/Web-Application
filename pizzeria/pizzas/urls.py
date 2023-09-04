2bedbe5c14c455046a9d9f76835642157ece4c6a
jrry8 <31778235+jrry8@users.noreply.github.com>
2023-09-03 21:12:25 -0700

    added page showing toppings for a pizza

--- pizzeria/pizzas/urls.py
+++ pizzeria/pizzas/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     re_path(r'^$', views.index, name='index'),
     # show all pizzas
     re_path(r'^pizzas/$', views.pizzas, name='pizzas'),
+    # detail toppings for a pizza
+    re_path(r'^pizzas/(?P<pizza_id>\d+)/$', views.pizza, name='pizza'),
 ]
--- pizzeria/pizzas/views.py
+++ pizzeria/pizzas/views.py
@@ -8,3 +8,10 @@ def pizzas(request):
     pizzas = Pizza.objects.order_by('name')
     context = {'pizzas': pizzas}
     return render(request, 'pizzas/pizzas.html', context)
+
+# show a single pizza and all its toppings
+def pizza(request, pizza_id):
+    pizza = Pizza.objects.get(id=pizza_id)
+    toppings = pizza.topping_set.order_by('name')
+    context = {'pizza': pizza, 'toppings': toppings}
+    return render(request, 'pizzas/pizza.html', context)
