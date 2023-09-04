831f5af9bdad72b63108626ffe6dcab2576c6e8a
jrry8 <31778235+jrry8@users.noreply.github.com>
2023-09-03 22:08:05 -0700

    added a page for adding new topics

--- ll/learning_logs/urls.py
+++ ll/learning_logs/urls.py
@@ -11,4 +11,6 @@ urlpatterns = [
     re_path(r'^topics/$', views.topics, name='topics'),
     # detail page for a single topic
     re_path(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic'),
+    # page for adding a new topic
+    re_path(r'^new_topic/$', views.new_topic, name='new_topic'),
 ]
--- ll/learning_logs/views.py
+++ ll/learning_logs/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 from .models import Topic
+from .forms import TopicForm
 
 # home page for learning log
 def index(request):
     return render(request, 'learning_logs/index.html')
 
+# display all topics
 def topics(request):
     topics = Topic.objects.order_by('date_added')
     context = {'topics': topics}
@@ -17,3 +21,18 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
+
+# add a new topic
+def new_topic(request):
+    if request.method != 'POST':
+        # no data submitted; create a blank form.
+        form = TopicForm()
+    else:
+        # POST data submitted; process data
+        form = TopicForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('learning_logs:topics'))
+
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
