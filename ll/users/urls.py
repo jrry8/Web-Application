7221a8132eb5a46d8a33da00b66d71e5b4b342a2
jrry8 <31778235+jrry8@users.noreply.github.com>
2023-09-05 10:11:31 -0700

    added login page

---
+++ ll/users/urls.py
@@ -0,0 +1,10 @@
+from django.urls import re_path
+from django.contrib.auth.views import LoginView
+
+from . import views
+
+app_name = 'users'
+urlpatterns = [
+    # login page
+    re_path(r'^login/$', LoginView.as_view(template_name='users/login.html'), name='login'),
+]
