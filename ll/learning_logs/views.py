e88fd614ce3a057fb2895eda1edcdbd6b4c7aa83
jrry8 <31778235+jrry8@users.noreply.github.com>
2023-09-07 12:33:46 -0700

    restricted topics access to appropriate users

--- ll/learning_logs/views.py
+++ ll/learning_logs/views.py
@@ -13,7 +13,7 @@ def index(request):
 # display all topics
 @login_required
 def topics(request):
-    topics = Topic.objects.order_by('date_added')
+    topics = Topic.objects.filter(owner=request.user).order_by('date_added')
     context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
 
