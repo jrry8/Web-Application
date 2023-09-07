30fd1e7bd63277690108294f04cb85755aacea1a
jrry8 <31778235+jrry8@users.noreply.github.com>
2023-09-07 12:37:36 -0700

    raise 404 if user tries to access a topic they don't own

--- ll/learning_logs/views.py
+++ ll/learning_logs/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, Http404
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 
@@ -21,6 +21,9 @@ def topics(request):
 @login_required
 def topic(request, topic_id):
     topic = Topic.objects.get(id=topic_id)
+    # check that the topic belongs to the current user
+    if topic.owner != request.user:
+        raise Http404
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
