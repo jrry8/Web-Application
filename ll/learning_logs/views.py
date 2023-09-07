a58dc0d92aa33fe42a18e467bf4eb08512f1cae7
jrry8 <31778235+jrry8@users.noreply.github.com>
2023-09-07 13:12:20 -0700

    protected new_entry from unauthorized access

--- ll/learning_logs/views.py
+++ ll/learning_logs/views.py
@@ -53,6 +53,7 @@ def new_topic(request):
 @login_required
 def new_entry(request, topic_id):
     topic = Topic.objects.get(id=topic_id)
+    check_topic_owner(topic.owner, request.user)
     if request.method != 'POST':
         form = EntryForm()
     else:
