3272f8b9b18cd0a2a4a8e27183bd2a1e227e0c15
jrry8 <31778235+jrry8@users.noreply.github.com>
2023-09-07 12:03:05 -0700

    restricted access to topics page to only registered users

--- ll/learning_logs/views.py
+++ ll/learning_logs/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
@@ -10,6 +11,7 @@ def index(request):
     return render(request, 'learning_logs/index.html')
 
 # display all topics
+@login_required
 def topics(request):
     topics = Topic.objects.order_by('date_added')
     context = {'topics': topics}
