85a9ab86c1cd681fdd7b5b3d6bdf0d2f05b48f1b
jrry8 <31778235+jrry8@users.noreply.github.com>
2023-09-07 13:09:20 -0700

    refactored checking topic owner

--- ll/learning_logs/views.py
+++ ll/learning_logs/views.py
@@ -22,8 +22,7 @@ def topics(request):
 def topic(request, topic_id):
     topic = Topic.objects.get(id=topic_id)
     # check that the topic belongs to the current user
-    if topic.owner != request.user:
-        raise Http404
+    check_topic_owner(topic.owner, request.user)
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
@@ -75,8 +74,7 @@ def edit_entry(request, entry_id):
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
     # check that the entry's topic belongs to the current user
-    if topic.owner != request.user:
-        raise Http404
+    check_topic_owner(topic.owner, request.user)
     if request.method != 'POST':
         form = EntryForm(instance=entry)
     else:
@@ -86,3 +84,7 @@ def edit_entry(request, entry_id):
             return HttpResponseRedirect(reverse('learning_logs:topic', args=[topic.id]))
     context = {'entry': entry, 'topic': topic, 'form': form}
     return render(request, 'learning_logs/edit_entry.html', context)
+
+def check_topic_owner(owner, user):
+    if owner != user:
+        raise Http404
